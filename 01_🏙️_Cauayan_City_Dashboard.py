60bf0bf40875d8d8b04f5a40f6cece7d344a6317
ZayBen <personal.rbmz@gmail.com>
2022-11-09 11:42:03 +0800

    Update 01_🏙️_Cauayan_City_Dashboard.py

--- 01_🏙️_Cauayan_City_Dashboard.py
+++ 01_🏙️_Cauayan_City_Dashboard.py
@@ -6,7 +6,7 @@ st.caption('Made by Ronald Benz M. Zhang')
 
 st.markdown('This app allows users to access the data of the Cauayan City for academic and research purposes.')
 
-video_file = open('Cauayan City Dashboard Demo.webm', 'rb')
+video_file = open('Cauayan City Dashboard.webm', 'rb')
 
 video_bytes = video_file.read()
 
