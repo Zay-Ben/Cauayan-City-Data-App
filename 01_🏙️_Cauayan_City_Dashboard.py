00f9398f44728c48d5fe7f9899d0a47921b10136
Zay Ben <personal.rbmz@gmail.com>
2022-11-18 10:43:25 +0800

    Update 01_🏙️_Cauayan_City_Dashboard.py

--- 01_🏙️_Cauayan_City_Dashboard.py
+++ 01_🏙️_Cauayan_City_Dashboard.py
@@ -6,7 +6,7 @@ st.title('Cauayan City Dashboard')
 
 st.caption('Made by Ronald Benz M. Zhang')
 
-st.markdown('This app allows users to access the data of the Cauayan City for academic and research purposes.')
+st.text('This app allows users to access the data of the Cauayan City for academic and research purposes.')
 
 video_file = open('Cauayan City Dashboard.webm', 'rb')
 
